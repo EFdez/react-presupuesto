99770d13b439e89403598089b03a293b7060329a
Efdez <escarlafdez@gmail.com>
2020-09-10 00:29:10 +0200

    done

---
+++ src/components/Formulario.js
@@ -0,0 +1,86 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types'
+import shortid from 'shortid'
+import Error from './Error'
+
+
+const Formulario = ({ guardarGasto, guardarCrearGasto }) => {
+    const [nombre, guardarNombre] = useState('')
+    const [cantidad, guardarCantidad] = useState(0)
+    const [error, guardarError] = useState(false)
+
+    const agregarGasto = e => {
+        e.preventDefault()
+
+        //validación
+        if (cantidad < 1 || isNaN(cantidad) || nombre.trim() === '') {
+            guardarError(true)
+            return;
+        }
+        guardarError(false)
+
+        //construir gasto
+        const gasto = {
+            nombre,
+            cantidad,
+            id: shortid.generate()
+        }
+        console.log(gasto)
+
+        //pasar gasto al componente padre
+        guardarGasto(gasto)
+
+        guardarCrearGasto(true)
+
+        //resetar form
+        guardarNombre('')
+        guardarCantidad(0)
+    }
+
+    return (
+        <form
+            onSubmit={agregarGasto}
+        >
+            <h2>Agrega tus gastos aquí</h2>
+            {error ?
+                <Error mensaje="Ambos campos son obligatorios o Presupuesto incorrecto" />
+                :
+                null}
+            <div className="campo">
+                <label>Nombre Gasto</label>
+                <input
+                    type="text"
+                    className="u-full-width"
+                    placeholder="Ej. Comida"
+                    value={nombre}
+                    onChange={e => guardarNombre(e.target.value)}
+                />
+            </div>
+            <div className="campo">
+                <label>Cantidad Gasto</label>
+                <input
+                    type="number"
+                    className="u-full-width"
+                    placeholder="Ej. 100"
+                    value={cantidad}
+                    onChange={e => guardarCantidad(parseInt(e.target.value, 10))}
+                />
+            </div>
+
+            <input
+                type="submit"
+                className="button-primary u-full-width"
+                value="Agregar gasto"
+            />
+
+        </form>
+
+    );
+}
+
+Formulario.propTypes = {
+    guardarGasto: PropTypes.func.isRequired,
+    guardarCrearGasto: PropTypes.func.isRequired
+}
+
+export default Formulario;
